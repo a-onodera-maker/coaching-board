fed1e038cea52042f3ff56b67799c31b538b2f7b
小野寺章彦 <a_onodera@onoderaakihikonoMacBook-Air.local>
2026-07-28 14:52:11 +0900

    終わりにしたい

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1445850361" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3327367540" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
